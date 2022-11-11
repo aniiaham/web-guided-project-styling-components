c8dc06d0b057bcf1aea34d9157325b1e44d772b4
aniiaham <aniya_axm@icloud.com>
2022-11-10 22:25:23 -0500

    finished project'

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,12 +1,58 @@
 import React from 'react'
+import styled, { keyframes } from 'styled-components'
 
-export default function Friend({ info, action }) {
+const kf = keyframes`
+  100% {
+    opacity: 1;
+  }
+`
+//OUTSIDE OF THE FRIEND COMPONENT 
+//choosing DIV bec top level element is DIV 
+//CAPITALIZE THE STYLE COMPONENT 
+//DON'T FORGER THE SEMICOLONS
+
+const StyledFriend = styled.div`
+  opacity: 0;
+  animation: ${kf} 0.5s ease-in-out forwards;
+  color: ${pr => pr.danger ? pr.theme.dangerColor : pr.theme.primaryColor
+  };
+  font-weight: ${pr => pr.bold? 'bold' : 'initial'};
+  width: 60%;
+  display: flex;
+  justify-content: space-between;
+
+  @media (max-width: 550px) {
+    width: 100%;
+  }
+  
+  button{
+    color: ${pr => pr.theme.tertiaryColor};
+    &:hover {
+       transform: scale(1.5);
+       transition: all 0.5s ease-in-out;
+    }
+    transition: all 0.5s ease-in-out;
+  }
+
+  &:hover {
+    color: ${pr => pr.theme.secondaryColor};
+    background-color: cyan;
+    transition: all 0.5s ease-in-out;
+  }
+  transition: all 0.5s ease-in-out;
+`
+
+// const Button = styled.button`
+//   color: pink;
+//   `
+export default function Friend(props) {
+  const { bold, info, action } = props  
   return (
-    <div className='friend'>
+    <StyledFriend bold={bold} danger={info.name === 'Bea'}>
       {info.name}
       <button onClick={() => action(info.id)}>
         See details
       </button>
-    </div>
+    </StyledFriend>
   )
 }
